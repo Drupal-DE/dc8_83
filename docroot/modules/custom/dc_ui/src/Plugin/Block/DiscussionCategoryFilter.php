e9a6b4f63e2c0e69d57229eb58ff275dc03a9889
Stefan Borchert <stefan.borchert@undpaul.de>
2017-01-28 11:22:29 +0100

    #16: update category filter
    Signed-off-by: Stefan Borchert <stefan.borchert@undpaul.de>

--- docroot/modules/custom/dc_ui/src/Plugin/Block/DiscussionCategoryFilter.php
+++ docroot/modules/custom/dc_ui/src/Plugin/Block/DiscussionCategoryFilter.php
@@ -5,6 +5,7 @@ namespace Drupal\dc_ui\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\taxonomy\TermInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -55,24 +56,58 @@ class DiscussionCategoryFilter extends BlockBase implements ContainerFactoryPlug
   public function build() {
     /* @var $storage \Drupal\taxonomy\TermStorageInterface */
     $storage = $this->entityManager->getStorage('taxonomy_term');
+    $active_tid = \Drupal::routeMatch()->getParameter('tid');
+    $active_trail = $active_tid ? $storage->loadAllParents($active_tid) : [];
+
     $categories = [];
     $top_level_terms = $storage->loadTree('discussion_category', 0, 1, TRUE);
-    /* @var $term \Drupal\taxonomy\TermInterface */
+    /* @var $term TermInterface */
     foreach ($top_level_terms as $term) {
-      $tid = $term->id();
-      $children = $storage->loadChildren($tid);
-      $categories[$tid] = [
+      $categories[$term->id()] = [
         'term' => $term,
-        'children' => $children,
+        'children' => $this->buildChildren($term, $active_trail),
       ];
     }
 
     $build = [
       '#theme' => 'dc_category_filter',
       '#categories' => $categories,
+      '#active' => $active_trail,
     ];
 
     return $build;
+  }
+
+  /**
+   * Build child list of category.
+   *
+   * @param \Drupal\taxonomy\TermInterface $term
+   *   Term to build child list for.
+   * @param array $active_tree
+   *   List of active terms.
+   *
+   * @return array
+   *   List of children to render.
+   */
+  protected function buildChildren(TermInterface $term, array $active_tree) {
+    /* @var $storage \Drupal\taxonomy\TermStorageInterface */
+    $storage = $this->entityManager->getStorage('taxonomy_term');
+    $terms = $storage->loadChildren($term->id());
+    if (empty($terms)) {
+      return [];
+    }
+
+    $children = [];
+    /* @var $child_term \Drupal\taxonomy\TermInterface */
+    foreach ($terms as $tid => $child_term) {
+      $children[$tid] = [
+        'term' => $child_term,
+        'active' => in_array($tid, array_keys($active_tree)),
+        'children' => $this->buildChildren($child_term, $active_tree),
+      ];
+    }
+
+    return $children;
   }
 
 }
