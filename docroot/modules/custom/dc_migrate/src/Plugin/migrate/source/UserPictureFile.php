3c9253a43f465562ee78c72d908efa3026cc7cb6
Stefan Borchert <stefan.borchert@undpaul.de>
2017-02-01 20:18:19 +0100

    #14: migrate user pictures
    Signed-off-by: Stefan Borchert <stefan.borchert@undpaul.de>

--- docroot/modules/custom/dc_migrate/src/Plugin/migrate/source/UserPictureFile.php
+++ docroot/modules/custom/dc_migrate/src/Plugin/migrate/source/UserPictureFile.php
@@ -4,6 +4,7 @@ namespace Drupal\dc_migrate\Plugin\migrate\source;
 
 use Drupal\Core\Database\Query\SelectInterface;
 use Drupal\dc_migrate\Plugin\migrate\source\DcSqlBaseFile;
+use Drupal\migrate\Row;
 
 /**
  * SQL-based source plugin for user pictures.
@@ -17,9 +18,51 @@ class UserPictureFile extends DcSqlBaseFile {
   /**
    * {@inheritdoc}
    */
-  protected function alterQuery(SelectInterface $query) {
-    // Add join to table of field "users".
-    $query->join('users', 'u', 'fm.fid = u.picture');
+  public function query() {
+    // Rewrite query.
+    $query = $this->select('users', 'u');
+    $query->addField('u', 'uid');
+    $query->addField('u', 'picture', 'uri');
+    $query->condition('u.picture', '', '<>');
+    $query->isNotNull('u.picture');
+
+    return $query->distinct()->orderBy('u.uid');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return [
+      'uid' => $this->t('File owner'),
+      'filename' => $this->t('Name of file'),
+      'filepath' => $this->t('File path'),
+      'directory' => $this->t('File directory'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    if (!parent::prepareRow($row)) {
+      return FALSE;
+    }
+    // Extract file name.
+    $path = $row->getSourceProperty('filepath');
+    $row->setSourceProperty('filename', substr($path, strrpos($path, '/') + 1));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'uid' => [
+        'type' => 'integer',
+        'alias' => 'u',
+      ],
+    ];
   }
 
 }
