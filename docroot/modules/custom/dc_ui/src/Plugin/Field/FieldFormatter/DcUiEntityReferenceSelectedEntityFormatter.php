0600e8c4f1d7f57c51aad996825da35c7faef19e
Stefan Borchert <stefan.borchert@undpaul.de>
2017-03-04 17:54:10 +0100

    #28: update custom entity reference formatter
    Signed-off-by: Stefan Borchert <stefan.borchert@undpaul.de>

--- docroot/modules/custom/dc_ui/src/Plugin/Field/FieldFormatter/DcUiEntityReferenceSelectedEntityFormatter.php
+++ docroot/modules/custom/dc_ui/src/Plugin/Field/FieldFormatter/DcUiEntityReferenceSelectedEntityFormatter.php
@@ -73,41 +73,18 @@ class DcUiEntityReferenceSelectedEntityFormatter extends EntityReferenceRevision
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $view_mode = $this->getSetting('view_mode');
     $index = $this->getSetting('index');
-    $elements = [];
 
     if ((-1 === $index) || (($selected_item = $items->get($index)) === NULL)) {
       // Display all items.
       return parent::viewElements($items, $langcode);
     }
 
+    // Limit list of items to selected.
     $items_reduced = new EntityReferenceFieldItemList($items->getDataDefinition());
     $items_reduced->set(0, $selected_item);
 
-    foreach ($this->getEntitiesToView($items_reduced, $langcode) as $delta => $entity) {
-      // Protect ourselves from recursive rendering.
-      static $depth = 0;
-      $depth++;
-      if ($depth > 20) {
-        $this->loggerFactory
-          ->get('entity')
-          ->error('Recursive rendering detected when rendering entity @entity_type @entity_id. Aborting rendering.', ['@entity_type' => $entity->getEntityTypeId(), '@entity_id' => $entity->id()]);
-        return $elements;
-      }
-      $view_builder = \Drupal::entityTypeManager()->getViewBuilder($entity->getEntityTypeId());
-      $elements[$delta] = $view_builder->view($entity, $view_mode, $entity->language()->getId());
-
-      // Add a resource attribute to set the mapping property's value to the
-      // entity's url. Since we don't know what the markup of the entity will
-      // be, we shouldn't rely on it for structured data such as RDFa.
-      if (!empty($items[$delta]->_attributes) && !$entity->isNew() && $entity->hasLinkTemplate('canonical')) {
-        $items[$delta]->_attributes += ['resource' => $entity->toUrl()->toString()];
-      }
-      $depth = 0;
-    }
-
-    return $elements;
+    return parent::viewElements($items_reduced, $langcode);
   }
 
 }
